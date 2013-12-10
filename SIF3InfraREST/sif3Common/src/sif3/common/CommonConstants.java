c8d5f4dbaa298dca12654c7fad33816f37b8e1d1
Joerg Huber <joerg.huber@systemic.com.au>
2013-12-10 14:37:02 +0800

    Initial submission.

---
+++ SIF3InfraREST/sif3Common/src/sif3/common/CommonConstants.java
@@ -0,0 +1,30 @@
+/*
+ * CommonConstants.java
+ * Created: 05/12/2013
+ *
+ * Copyright 2013 Systemic Pty Ltd
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software distributed under the License 
+ * is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
+ * or implied.
+ * See the License for the specific language governing permissions and limitations under the License.
+ */
+
+package sif3.common;
+
+/**
+ * This file holds the values of some system wide constants.
+ * 
+ * @author Joerg Huber
+ */
+public class CommonConstants
+{
+	/* Name of environment property file without the file extension */
+	public static final String ENV_PROP_FILE_NAME = "environment";
+}
