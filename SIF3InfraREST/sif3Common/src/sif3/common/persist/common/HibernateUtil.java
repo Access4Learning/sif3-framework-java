80c6b9f31686854da733ba23d556d371f17a4ef7
Joerg Huber <joerg.huber@systemic.com.au>
2014-11-27 14:00:33 +0800

    Check if connection pool is initialised successfully.

--- SIF3InfraREST/sif3Common/src/sif3/common/persist/common/HibernateUtil.java
+++ SIF3InfraREST/sif3Common/src/sif3/common/persist/common/HibernateUtil.java
@@ -1,22 +1,52 @@
 package sif3.common.persist.common;
 
+import org.apache.log4j.Logger;
 import org.hibernate.Hibernate;
+import org.hibernate.Session;
 import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
 import org.hibernate.cfg.Configuration;
 
 public class HibernateUtil
 {
-	private static SessionFactory sessionFactory;
+	protected static final Logger logger = Logger.getLogger(HibernateUtil.class);
+	
+	private static SessionFactory sessionFactory = null;
 
 	static
 	{
+		Session session = null;
+		Transaction tx = null;
 		try
 		{
-		    sessionFactory = new  Configuration().configure("sif3infra.hibernate.cfg.xml").buildSessionFactory();		
+		    sessionFactory = new  Configuration().configure("sif3infra.hibernate.cfg.xml").buildSessionFactory();
+		    
+		    //even if the above fails, i.e. cannot allocate a connection then no error is reported and the 
+		    //sessionFactory is created. The only way to find out if it succeeded is by trying to get a connection.
+		    //So let's try this here and if no connection can be allocated then report the error that way.
+		    session = getSessionFactory().openSession();
+		    
+		    // Getting a transaction will throw an exception if hibernate had an issue allocating a connection pool.
+		    tx = session.beginTransaction();		    
 		}
 		catch (Exception e)
 		{
-			e.printStackTrace();
+			logger.error("Failed to initialise hibernate and connection pool: "+e.getMessage(), e);
+			sessionFactory = null;
+		}
+		finally 
+		{
+			// Close test session and connection.
+			if ((tx != null) && tx.isActive())
+			{
+				tx.rollback();
+				tx = null;
+			}
+			if (session != null)
+			{
+				session.close();
+				session = null;
+			}
 		}
 	}
 
@@ -34,7 +64,10 @@ public class HibernateUtil
 	{
 		try
 		{
-			getSessionFactory().close();
+			if (getSessionFactory() != null)
+			{
+				getSessionFactory().close();
+			}
 		}
 		catch (Exception ex) {} // nothing we can really do!
 	}
