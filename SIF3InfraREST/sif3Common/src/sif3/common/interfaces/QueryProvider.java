c75cf7b564176459870dbfe541ef8138fca37826
Joerg Huber <joerg.huber@systemic.com.au>
2015-01-20 11:22:44 +0800

    Formatted and added Disclaimer.

--- SIF3InfraREST/sif3Common/src/sif3/common/interfaces/QueryProvider.java
+++ SIF3InfraREST/sif3Common/src/sif3/common/interfaces/QueryProvider.java
@@ -1,3 +1,20 @@
+/*
+ * QueryProvider.java
+ * Created: 05/01/2015
+ *
+ * Copyright 2015 Systemic Pty Ltd
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software distributed under the License 
+ * is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
+ * or implied.
+ * See the License for the specific language governing permissions and limitations under the License.
+ */
 package sif3.common.interfaces;
 
 import sif3.common.exception.PersistenceException;
@@ -11,20 +28,26 @@ import sif3.common.model.SIFZone;
 /**
  * This interface is an extension to the Provider interface to also allow object queries.
  */
-public interface QueryProvider extends Provider {
-  
-  /**
-   * This method is used to retrieve any number of objects. Filtered by the service path.
-   * Each predicate within the criteria holds a parent object and it's key.
-   * @param queryCriteria
-   * @param zone
-   * @param context
-   * @param pagingInfo
-   * @param metadata
-   * @return
-   * @throws PersistenceException
-   * @throws UnsupportedQueryException
-   */
-  public Object retrieveByServicePath(QueryCriteria queryCriteria, SIFZone zone, SIFContext context, PagingInfo pagingInfo, RequestMetadata metadata) throws PersistenceException, UnsupportedQueryException;
-  
+public interface QueryProvider extends Provider
+{
+
+	/**
+	 * This method is used to retrieve any number of objects. Filtered by the service path. Each
+	 * predicate within the criteria holds a parent object and it's key.
+	 * 
+	 * @param queryCriteria
+	 * @param zone
+	 * @param context
+	 * @param pagingInfo
+	 * @param metadata
+	 * @return
+	 * @throws PersistenceException
+	 * @throws UnsupportedQueryException
+	 */
+	public Object retrieveByServicePath(QueryCriteria queryCriteria, 
+			                            SIFZone zone, 
+			                            SIFContext context, 
+			                            PagingInfo pagingInfo, 
+			                            RequestMetadata metadata) throws PersistenceException, UnsupportedQueryException;
+
 }
--- SIF3InfraREST/sif3Common/src/sif3/common/model/QueryCriteria.java
+++ SIF3InfraREST/sif3Common/src/sif3/common/model/QueryCriteria.java
@@ -1,56 +1,84 @@
+/*
+ * QueryCriteria.java
+ * Created: 05/01/2015
+ *
+ * Copyright 2015 Systemic Pty Ltd
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software distributed under the License 
+ * is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
+ * or implied.
+ * See the License for the specific language governing permissions and limitations under the License.
+ */
 package sif3.common.model;
 
 import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.List;
 
-public class QueryCriteria implements Serializable {
+public class QueryCriteria implements Serializable
+{
 
-  private static final long serialVersionUID = 1803812315289373807L;
+	private static final long serialVersionUID = 1803812315289373807L;
 
-  private QueryJoinType joinType;
-  private List<QueryPredicate> predicates = new ArrayList<QueryPredicate>();
-  
-  public QueryCriteria() {
-    joinType = QueryJoinType.AND;
-  }
-  
-  public QueryCriteria(QueryJoinType joinType) {
-    this.joinType = joinType;
-  }
+	private QueryJoinType joinType;
+	private List<QueryPredicate> predicates = new ArrayList<QueryPredicate>();
 
-  public QueryJoinType getJoinType() {
-    return joinType;
-  }
+	public QueryCriteria()
+	{
+		joinType = QueryJoinType.AND;
+	}
 
-  public void setJoinType(QueryJoinType joinType) {
-    this.joinType = joinType;
-  }
+	public QueryCriteria(QueryJoinType joinType)
+	{
+		this.joinType = joinType;
+	}
 
-  public List<QueryPredicate> getPredicates() {
-    return predicates;
-  }
+	public QueryJoinType getJoinType()
+	{
+		return joinType;
+	}
 
-  public void setPredicates(List<QueryPredicate> predicates) {
-    this.predicates = predicates;
-  }
-  
-  public void addPredicate(QueryPredicate predicate) {
-    this.predicates.add(predicate);
-  }
+	public void setJoinType(QueryJoinType joinType)
+	{
+		this.joinType = joinType;
+	}
 
-  public int size() {
-    return predicates == null ? 0 : predicates.size();
-  }
-  
-  public String toString() {
-    String result = "";
-    String prefix = "";
-    for (int i = 0; predicates != null && i < predicates.size(); i++) {
-      QueryPredicate predicate = predicates.get(i);
-      result += prefix + predicate.toString();
-      prefix = " " + joinType.getName() + " ";
-    }
-    return result;
-  }
+	public List<QueryPredicate> getPredicates()
+	{
+		return predicates;
+	}
+
+	public void setPredicates(List<QueryPredicate> predicates)
+	{
+		this.predicates = predicates;
+	}
+
+	public void addPredicate(QueryPredicate predicate)
+	{
+		this.predicates.add(predicate);
+	}
+
+	public int size()
+	{
+		return predicates == null ? 0 : predicates.size();
+	}
+
+	public String toString()
+	{
+		String result = "";
+		String prefix = "";
+		for (int i = 0; predicates != null && i < predicates.size(); i++)
+		{
+			QueryPredicate predicate = predicates.get(i);
+			result += prefix + predicate.toString();
+			prefix = " " + joinType.getName() + " ";
+		}
+		return result;
+	}
 }
