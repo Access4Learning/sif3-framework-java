b02ec0a655d1da04488702fdaab0b6e34d90641b
Joerg Huber <joerg.huber@systemic.com.au>
2015-05-12 11:48:49 +0800

    Updated javadoc.

--- SIF3InfraREST/sif3Common/src/sif3/common/interfaces/SecurityService.java
+++ SIF3InfraREST/sif3Common/src/sif3/common/interfaces/SecurityService.java
@@ -42,11 +42,12 @@ public interface SecurityService
 	/**
 	 * This method return true if the given 'securityToken' is a valid token for the given security service. The security service can be any service
 	 * such as OAuth, LDAP, ActiveDirectory, OpenID etc. It is up to the implementor of this interface to interact with the appropriate security
-	 * service to determine if the given securityToken is valid. Reasons for a token to not be valid include but are not limited to, expried tokens, 
+	 * service to determine if the given securityToken is valid. Reasons for a token to not be valid include but are not limited to, expired tokens, 
 	 * incorrect tokens, not authenticated tokens etc.
 	 *     
 	 * @param securityToken The token that shall be validated against a given security service such as LDAP, OAuth, Active Directory, etc.
-	 * @param requestMetadata Metadata that has been sourced from a request. 
+	 * @param requestMetadata Metadata that has been sourced from a request. The environmentID property is always null because the token is
+     *                        not yet authenticated and therefore the environment is not yet determined. 
 	 * 
 	 * @return TRUE if the token is known and valid to the security server and not expired. If a token is expired then FALSE should be returned.
 	 */
@@ -56,13 +57,14 @@ public interface SecurityService
 	 * This method may contact the security server which can be an OAuth server, and LDAP server a Active Directory etc. In return it will provide 
 	 * information that relate to the securityToken such as: <br/>
 	 * a) Information about the application and/or user of that securityToken (appUserInfo property populated in the TokenInfo) or
-	 * b) Information about the SIF environment or SIF session the securityToken relates to. This would be the case for alredy existing SIF 
+	 * b) Information about the SIF environment or SIF session the securityToken relates to. This would be the case for already existing SIF 
 	 *    Environments.
 	 * Further an expire date might be set for the securityToken if the token has expired. If the securityToken does not expire then the 
 	 * expire date is null in the returned TokenInfo object.
 	 * 
 	 * @param securityToken The security token for which the TokenInfo shall be returned.
-	 * @param requestMetadata Metadata that has been sourced from a request. 
+	 * @param requestMetadata Metadata that has been sourced from a request. The environmentID property is always null because the token is
+     *                        not yet authenticated and therefore the environment is not yet determined. 
 	 * 
 	 * @return See Desc. It is expected that this method only returns either the environmentKey or the SIF environment ID or the SIF session token but
 	 *         not all of these at the same time.
