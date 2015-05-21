bda5b27256ba41ac38b440e9334d48bbff685525
Joerg Huber <joerg.huber@systemic.com.au>
2015-05-21 11:24:09 +0800

    Added URL Query Parameter and HTTP Header property.

--- SIF3InfraREST/sif3Common/src/sif3/common/model/RequestMetadata.java
+++ SIF3InfraREST/sif3Common/src/sif3/common/model/RequestMetadata.java
@@ -18,6 +18,7 @@
 
 package sif3.common.model;
 
+import sif3.common.header.HeaderProperties;
 import sif3.common.header.HeaderValues.QueryIntention;
 
 
@@ -31,16 +32,16 @@ public class RequestMetadata extends BaseMetadata
     
     private QueryIntention queryIntention = null; // May only be used in HTTP GET requests
     private String navigationID = null; // May only be used in HTTP GET requests
-    private String sourceName = null; // May only be used in brokered environment! Broker will assign this.
     
-    // The following is not stored in a HTTP header of the request. It is a useful bit of data for the provider
-    // in a DIRECT environment as it uniquely identifies the consumer's environment. In a BROKERED environment this
-    // will always be the environment ID of the provider in relation to the broker.
+    // Refer to javadoc for the getSourceName() method.
+    private String sourceName = null; 
+    
+    // Refer to javadoc for the getEnvironmentID() method.
     private String environmentID = null; // Only useful in DIRECT environments
     
-	public RequestMetadata()
+	public RequestMetadata(URLQueryParameter queryParams, HeaderProperties httpHeaderParams)
     {
-    	super();
+    	super(queryParams, httpHeaderParams);
     }
     
 	public QueryIntention getQueryIntention()
@@ -63,6 +64,19 @@ public class RequestMetadata extends BaseMetadata
     	this.navigationID = navigationID;
     }
 	
+	/**
+	 * IMPORTANT:<br>
+     * This value is assigned by the broker in a brokered environment. It is something that identifies the source
+     * of the request. It could be the 'applicationKey' of the consumer's environment but this is not mandated.
+     * It might be a different value and should not be used to make 'critical' decisions about the provider's 
+     * behaviour.
+     *
+     * In a DIRECT environment the SIF3 Framework implementation will assign the 'applicationKey' of the consumer's
+     * environment. This is an arbitrary decision that is true for this framework. Similar as with the brokered
+     * environment a provider should not base 'critical' decisions based on this value.
+     * 
+     * @return See desc.
+	*/
 	public String getSourceName()
     {
     	return this.sourceName;
@@ -74,9 +88,15 @@ public class RequestMetadata extends BaseMetadata
     }
 
 	/**
-	 * IMPORTANT: This value is only really of any use in a DIRECT environment where this environmentID is the
-	 *            environmentID of the consumer. In a BROKERED environment this ID is the ID of the provider
-	 *            environment to the broker and is always the same. 
+	 * IMPORTANT:<br/>
+	 * The environmentID is not stored in a HTTP header of the request. It could be a useful bit of data for the 
+	 * provider in a DIRECT environment as it uniquely identifies the consumer's environment. This is a property
+	 * that is provided through this SIF3 Framework only. It is NOT a SIF 3.x specified header or property in any
+	 * way.<br/>
+	 * If the provider is acting in a BROKERED environment then the environmentID in this property is the ID of 
+	 * the provider's environment to the broker and is always the same.<br/><br/>
+	 * 
+	 * It is therefore important that this value is only used as a 'decision' making property in a DIRECT environment.
 	 *            
 	 * @return See Desc. Can be null in some cases where the environment cannot yet be determined (i.e before
 	 *         request is fully authenticated). 
