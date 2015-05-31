28ffb055118749950e35460ea8589a92440b1635
Joerg Huber <joerg.huber@systemic.com.au>
2015-05-31 10:56:03 +0800

    Added new parameter to some methods to enable custom HTTP and URL query parameters.

--- SIF3InfraREST/sif3Common/src/sif3/common/interfaces/EventProvider.java
+++ SIF3InfraREST/sif3Common/src/sif3/common/interfaces/EventProvider.java
@@ -18,6 +18,7 @@
 
 package sif3.common.interfaces;
 
+import sif3.common.header.HeaderProperties;
 import sif3.common.model.SIFContext;
 import sif3.common.model.SIFEvent;
 import sif3.common.model.SIFZone;
@@ -43,8 +44,10 @@ public interface EventProvider<L> extends DataModelLink
     public SIFEventIterator<L> getSIFEvents();
     
 	/**
-	 * This method is called just before the event is sent. This allows the provider to modify the actual object data before it is sent.
-	 * The final object to be sent must then be returned.<br/>
+	 * This method is called just before the event is sent. This allows the provider to modify the actual object data before it 
+	 * is sent. The final object to be sent must then be returned. Additional custom HTTP header fields can be provided that 
+	 * are sent with the event. Note if custom HTTP header fields are provided they might be overridden if they have the same
+	 * name as a SIF3 specified HTTP header.<br/>
 	 * The intend of this method is that the provider can change the event based on the zone and context this is sent to. A SIF event for
 	 * a particular SIF Object might be sent to more than one zone and context and for each of them the data might need to be altered before
 	 * it is sent.<br/><br/>
@@ -55,10 +58,11 @@ public interface EventProvider<L> extends DataModelLink
 	 * @param sifEvent The event object for which the events will be sent.
 	 * @param zone The zone the events will be sent to. This parameter value can be null which indicated the default zone.
 	 * @param context The context for which the events apply to. This parameter value can be null which indicated the default context.
+	 * @param customHTTPHeaders HTTP header values to be added to the publishing of an event.
 	 * 
 	 * @return See desc.
 	 */
-	public SIFEvent<L> modifyBeforePublishing(SIFEvent<L> sifEvent, SIFZone zone, SIFContext context);
+	public SIFEvent<L> modifyBeforePublishing(SIFEvent<L> sifEvent, SIFZone zone, SIFContext context, HeaderProperties customHTTPHeaders);
 
     
     /**
