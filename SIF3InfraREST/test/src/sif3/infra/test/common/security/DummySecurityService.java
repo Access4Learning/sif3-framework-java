41fbb011b6c95c3b9f74975a57c8d60e024a143f
Joerg Huber <joerg.huber@systemic.com.au>
2015-08-04 11:26:09 +0800

    Added test cases.

--- SIF3InfraREST/test/src/sif3/infra/test/common/security/DummySecurityService.java
+++ SIF3InfraREST/test/src/sif3/infra/test/common/security/DummySecurityService.java
@@ -17,17 +17,15 @@
  */
 package sif3.infra.test.common.security;
 
-import java.util.Date;
-
-import au.com.systemic.framework.utils.AdvancedProperties;
-import sif3.common.model.EnvironmentKey;
 import sif3.common.model.RequestMetadata;
+import sif3.common.model.security.TokenCoreInfo;
 import sif3.common.model.security.TokenInfo;
 import sif3.common.security.AbstractSecurityService;
+import au.com.systemic.framework.utils.AdvancedProperties;
 
 public class DummySecurityService extends AbstractSecurityService
 {
-	private static final long MINUTE = 1000*60;
+//	private static final long MINUTE = 1000*60;
 
 	public DummySecurityService(AdvancedProperties properties)
 	{
@@ -39,11 +37,13 @@ public class DummySecurityService extends AbstractSecurityService
 	public TokenInfo getTokenInfo(String securityToken, RequestMetadata requestMetadata)
 	{
 		System.out.println("DummySecurityService.getTokenInfo() called for securityToken = "+securityToken+" and request metadata = "+requestMetadata);
-		long expireTime = MINUTE*5; // expire every x minute.
-		TokenInfo tokenInfo = new TokenInfo(securityToken);
+		TokenInfo tokenInfo = BaseSecurityOperations.getTokenInfo(securityToken);
+		
+//		long expireTime = MINUTE*5; // expire every x minute.
+//		TokenInfo tokenInfo = new TokenInfo(securityToken);
 //		tokenInfo.setTokenExpiryDate(new Date(((new Date()).getTime()) + expireTime));
 //		tokenInfo.setSessionToken("80404112-96f7-4b2e-a118-3db458608877");
-		tokenInfo.setAppUserInfo(new EnvironmentKey(null, "BearerTest"));
+//		tokenInfo.setAppUserInfo(new EnvironmentKey(null, "BearerTest"));
 //		tokenInfo.setAppUserInfo(new EnvironmentKey(null, "BearerTest1"));
 		return tokenInfo;
 	}
@@ -55,4 +55,11 @@ public class DummySecurityService extends AbstractSecurityService
 		return true;
 	}
 
+    @Override
+    public TokenInfo generateToken(TokenCoreInfo coreInfo, String password)
+    {
+        System.out.println("TokenCoreInfo: "+coreInfo);
+        System.out.println("Value for prop2: "+coreInfo.getOtherInfo().get("prop2"));
+        return BaseSecurityOperations.createToken(coreInfo, getServiceProperties().getPropertyAsString("env.pwd", null));
+    }
 }
--- SIF3InfraREST/test/src/sif3/infra/test/common/security/TestSecurityServiceFactory.java
+++ SIF3InfraREST/test/src/sif3/infra/test/common/security/TestSecurityServiceFactory.java
@@ -1,60 +1,79 @@
 /*
- * TestSecurityServiceFactory.java
- * Created: 15/12/2014
- *
+ * TestSecurityServiceFactory.java Created: 15/12/2014
+ * 
  * Copyright 2014 Systemic Pty Ltd
  * 
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *    http://www.apache.org/licenses/LICENSE-2.0
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
+ * in compliance with the License. You may obtain a copy of the License at
  * 
- * Unless required by applicable law or agreed to in writing, software distributed under the License 
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software distributed under the License
  * is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
- * or implied.
- * See the License for the specific language governing permissions and limitations under the License.
+ * or implied. See the License for the specific language governing permissions and limitations under
+ * the License.
  */
 package sif3.infra.test.common.security;
 
+import sif3.common.model.EnvironmentKey;
+import sif3.common.model.security.TokenCoreInfo;
+import sif3.common.model.security.TokenInfo;
 import sif3.common.security.AbstractSecurityService;
 import sif3.common.security.BearerSecurityFactory;
 import au.com.systemic.framework.utils.AdvancedProperties;
 import au.com.systemic.framework.utils.PropertyManager;
 
-
 public class TestSecurityServiceFactory
 {
-  private static final String PROP_FILE_NAME = "StudentProvider";
-  
-  private AdvancedProperties properties = null;
+    private static final boolean IS_PROVDER          = false;
 
-  public TestSecurityServiceFactory()
-  {
-    PropertyManager propMgr = PropertyManager.getInstance();
-    propMgr.loadPropertyFile(PROP_FILE_NAME);
+    private static final String  PROV_PROP_FILE_NAME = "StudentProvider";
+    private static final String  CONS_PROP_FILE_NAME = "StudentConsumer";
 
-    properties = propMgr.getProperties(PROP_FILE_NAME);
-  }
-  
-  public void testGetInstance()
-  {
-    AbstractSecurityService service = BearerSecurityFactory.getSecurityService(properties);
-    if (service != null)
+    private AdvancedProperties   properties          = null;
+
+    public TestSecurityServiceFactory()
     {
-      service.validate("ABC", null);
+        PropertyManager propMgr = PropertyManager.getInstance();
+        String propFileName = IS_PROVDER ? PROV_PROP_FILE_NAME : CONS_PROP_FILE_NAME;
+        propMgr.loadPropertyFile(propFileName);
+
+        properties = propMgr.getProperties(propFileName);
     }
-  }
-  
-  public static void main(String[] args)
-  {
-    TestSecurityServiceFactory tester = new TestSecurityServiceFactory();
 
-    System.out.println("Start Testing TestSecurityServiceFactory...");
+    public void testGetInstance()
+    {
+    	AbstractSecurityService service = BearerSecurityFactory.getSecurityService(properties);
+        if (service != null)
+        {
+            if (IS_PROVDER)
+            {
+                TokenInfo tokenInfo = service.getInfo("VGVzdFNJUzoyMDE1LTA3LTE0VDE1OjE5OjAxWg==", null);
+                System.out.println(tokenInfo);
+            }
+            else
+            {
+                TokenCoreInfo coreInfo = new TokenCoreInfo();
+                coreInfo.setAppUserInfo(new EnvironmentKey(null, properties.getPropertyAsString("env.application.key", null)));
+//                TokenInfo tokenInfo = service.generateToken(coreInfo, properties.getPropertyAsString("env.pwd", null));
+                TokenInfo tokenInfo = service.createToken(coreInfo, properties.getPropertyAsString("env.pwd", null));
+                System.out.println(tokenInfo);
+            }
+        }
+        else
+        {
+            System.out.println("Returned Security Service is null.");
+        }
+    }
 
-    tester.testGetInstance();
+    public static void main(String[] args)
+    {
+        TestSecurityServiceFactory tester = new TestSecurityServiceFactory();
 
+        System.out.println("Start Testing TestSecurityServiceFactory...");
 
-    System.out.println("End Testing TestSecurityServiceFactory.");
-  }
+        tester.testGetInstance();
+
+        System.out.println("End Testing TestSecurityServiceFactory.");
+    }
 }
