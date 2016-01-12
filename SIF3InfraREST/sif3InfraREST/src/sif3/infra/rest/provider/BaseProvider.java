a2c0ee7a5c22cb8cd49245e57639406e13a43ca6
Joerg Huber <joerg.huber@systemic.com.au>
2016-01-12 09:09:57 +0800

    Handling of "Changes Since" calls from consumers.

--- SIF3InfraREST/sif3InfraREST/src/sif3/infra/rest/provider/BaseProvider.java
+++ SIF3InfraREST/sif3InfraREST/src/sif3/infra/rest/provider/BaseProvider.java
@@ -24,8 +24,16 @@ import java.util.TimerTask;
 import org.apache.log4j.Logger;
 
 import sif3.common.CommonConstants;
+import sif3.common.exception.DataTooLargeException;
+import sif3.common.exception.PersistenceException;
+import sif3.common.exception.UnsupportedQueryException;
+import sif3.common.header.HeaderProperties;
 import sif3.common.interfaces.EventProvider;
 import sif3.common.interfaces.Provider;
+import sif3.common.model.PagingInfo;
+import sif3.common.model.RequestMetadata;
+import sif3.common.model.SIFContext;
+import sif3.common.model.SIFZone;
 import sif3.infra.common.env.mgr.ProviderManagerFactory;
 import sif3.infra.common.env.types.EnvironmentInfo.EnvironmentType;
 import sif3.infra.common.env.types.ProviderEnvironment;
@@ -41,7 +49,7 @@ import au.com.systemic.framework.utils.AdvancedProperties;
  */
 public abstract class BaseProvider implements Provider, Runnable
 {
-	protected final Logger logger = Logger.getLogger(getClass());
+    protected final Logger logger = Logger.getLogger(getClass());
 
 	private Timer eventTimer = null;
 	private TimerTask eventTimerTask = null;
@@ -50,6 +58,27 @@ public abstract class BaseProvider implements Provider, Runnable
 	 * Shuts down the sub-class provider. This should release all associated resources with that provider.
 	 */
 	public abstract void shutdown();
+	
+	/**
+	 * This method behave like the getServiceInfo() method of the Provider Interface. Please refer to description in that class:<br/><br/>
+	 * 
+     * @see sif3.common.interfaces.Provider#getServiceInfo(sif3.common.model.SIFZone, sif3.common.model.SIFContext, sif3.common.model.PagingInfo, sif3.common.model.RequestMetadata)
+	 * 
+     * @param zone The Zone from which the request is being issued. Can be Null (default Zone)
+     * @param context The Context for which the objects shall be returned. Can be Null (default Zone)
+     * @param pagingInfo Page information to determine which results to return. Null = Return all (NOT RECOMMENDED!).
+     * @param metadata Metadata relating to the request. Note that most of the properties might be null.
+     * 
+     * @return Header Properties that shall be returned as HTTP Headers to the caller. Note this can be null or any number of
+     *         custom HTTP headers. Generally one would expect that to be null.
+     * 
+     * @throws UnsupportedQueryException The query provided with this request is not supported (NOT YET IMPLEMENTED FUNCTIONALITY)
+     * @throws PersistenceException Persistence Store could not be accessed successfully. An error log entry is performed and the 
+     *                              message of the exceptions holds some info.
+     * @throws DataTooLargeException If the data that shall be returned is too large due to the values in the paging info.
+	 */
+	public abstract HeaderProperties getCustomServiceInfo(SIFZone zone, SIFContext context, PagingInfo pagingInfo, RequestMetadata metadata) throws PersistenceException, UnsupportedQueryException, DataTooLargeException;
+	
 	
 	  /**
      */
@@ -149,6 +178,16 @@ public abstract class BaseProvider implements Provider, Runnable
     	}
     	
     	// Call finalise on sub-class.
+    }
+    
+    /* (non-Javadoc)
+     * @see sif3.common.interfaces.Provider#getServiceInfo(sif3.common.model.SIFZone, sif3.common.model.SIFContext, sif3.common.model.PagingInfo, sif3.common.model.RequestMetadata)
+     */
+    @Override
+    public HeaderProperties getServiceInfo(SIFZone zone, SIFContext context, PagingInfo pagingInfo, RequestMetadata metadata) throws PersistenceException, UnsupportedQueryException, DataTooLargeException
+    {
+        // For the time being we just call the abstract method getCustomServiceInfo().
+        return getCustomServiceInfo(zone, context, pagingInfo, metadata);
     }
 
     /*----------------------------------------*/
