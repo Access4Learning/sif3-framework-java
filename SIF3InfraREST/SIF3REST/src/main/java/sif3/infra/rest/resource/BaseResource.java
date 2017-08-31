32ee2b80bd71e7b2e6c4a6f8832b02039ca7c897
Joerg Huber <joerg.huber@systemic.com.au>
2017-08-31 15:52:33 +0800

    Use correct value for default context in log message.

--- SIF3InfraREST/SIF3REST/src/main/java/sif3/infra/rest/resource/BaseResource.java
+++ SIF3InfraREST/SIF3REST/src/main/java/sif3/infra/rest/resource/BaseResource.java
@@ -693,7 +693,7 @@ public abstract class BaseResource
 			if (!sif3Session.hasAccess(right, accessType, serviceName, zone, context))
 			{
 				String zoneID = (zone == null) ? "Default" : zone.getId();
-				String contextID = (context == null) ? "Default" : context.getId();
+				String contextID = (context == null) ? CommonConstants.DEFAULT_CONTEXT_NAME : context.getId();
 				error = new ErrorDetails(Status.FORBIDDEN.getStatusCode(), "Consumer is not authorized to issue the requested operation.", right.name()+ " access is not set to "+accessType.name()+" for the service "+serviceName+" and the given zone ("+zoneID+") and context ("+contextID+") in the environment "+sif3Session.getEnvironmentName(), "Provider side check.");			
 			}
 		}
