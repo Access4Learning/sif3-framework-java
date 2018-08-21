298545c62fe760ca5effc007f22b33c269b2969e
Joerg Huber <joerg.huber@systemic.com.au>
2018-08-21 10:50:53 +0800

    Extends SIFExcpetion.

--- SIF3InfraREST/sif3Common/src/main/java/sif3/common/exception/UnsupportedQueryException.java
+++ SIF3InfraREST/sif3Common/src/main/java/sif3/common/exception/UnsupportedQueryException.java
@@ -2,7 +2,7 @@
  * UnsupportedQueryException.java
  * Created: 23/09/2013
  *
- * Copyright 2013 Systemic Pty Ltd
+ * Copyright 2013-2018 Systemic Pty Ltd
  * 
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -18,27 +18,91 @@
 
 package sif3.common.exception;
 
-public class UnsupportedQueryException extends Exception
+import javax.ws.rs.core.Response.Status;
+
+import sif3.common.ws.ErrorDetails;
+
+/**
+ * Exception that can be used by providers if a service doesn't support a particular query type (i.e. QBE). It defaults the HTTP Status to 400
+ * (Bad Request) which indicates that the query is not supported.
+ * 
+ * @author Joerg Huber
+ *
+ */
+public class UnsupportedQueryException extends SIFException
 {
-  private static final long serialVersionUID = 907365345457L;
+    private static final long serialVersionUID = -7852913016152922737L;
 
-  public UnsupportedQueryException()
-  {
-      super();
-  }
+    /*
+     * The equivalent SIF Error will have the code & message set. Message is limited to 1024 Characters
+     */
+    public UnsupportedQueryException(String message)
+    {
+        this(message, null, null, null);
+    }
+    
+    /*
+     * The equivalent SIF Error will have the code & message set. Message is limited to 1024 Characters
+     */
+    public UnsupportedQueryException(String message, Throwable cause)
+    {
+        this(message, null, null, cause);
+    }
 
-  public UnsupportedQueryException(String msg)
-  {
-      super(msg);
-  }
+    /*
+     * The equivalent SIF Error will have the code, message & description set. Message is limited to 1024 Characters
+     */
+    public UnsupportedQueryException(String message, String description)
+    {
+        this(message, description, null, null);
+    }
+    
+    /*
+     * The equivalent SIF Error will have the code, message & description set. Message is limited to 1024 Characters
+     */
+    public UnsupportedQueryException(String message, String description, Throwable cause)
+    {
+        this(message, description, null, cause);
+    }
 
-  public UnsupportedQueryException(String msg, Throwable ex)
-  {
-      super(msg, ex);
-  }
-
-  public UnsupportedQueryException(Throwable ex)
-  {
-      super(ex);
-  }
+    /*
+     * The equivalent SIF Error will have the code, message, description & scope set. Message is limited to 1024 Characters
+     */
+    public UnsupportedQueryException(String message, String description, String scope)
+    {
+        this (message, description, scope, null);
+    }
+    
+    /*
+     * The equivalent SIF Error will have the code, message, description & scope set. Message is limited to 1024 Characters
+     */
+    public UnsupportedQueryException(String message, String description, String scope, Throwable cause)
+    {
+        super(new ErrorDetails(Status.BAD_REQUEST.getStatusCode(), message, description, scope), cause);
+    }
+    
+    
+//public class UnsupportedQueryException extends Exception
+//{
+//  private static final long serialVersionUID = 907365345457L;
+//
+//  public UnsupportedQueryException()
+//  {
+//      super();
+//  }
+//
+//  public UnsupportedQueryException(String msg)
+//  {
+//      super(msg);
+//  }
+//
+//  public UnsupportedQueryException(String msg, Throwable ex)
+//  {
+//      super(msg, ex);
+//  }
+//
+//  public UnsupportedQueryException(Throwable ex)
+//  {
+//      super(ex);
+//  }
 }
