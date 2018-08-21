8b3f9f2d727cc4527d43d628893d55ab55e5dddb
Joerg Huber <joerg.huber@systemic.com.au>
2018-08-21 10:38:57 +0800

    Added JOB tables to DB install scripts.

--- SIF3InfraREST/DB/DDL/Initial_Inserts.sql
+++ SIF3InfraREST/DB/DDL/Initial_Inserts.sql
@@ -5,3 +5,19 @@ insert into SIF3_ENV_TEMPLATE values ('DEV_LOCAL','devLocal.xml');
 
 insert into SIF3_APP_TEMPLATE (APP_TEMPLATE_ID,SOLUTION_ID,APPLICATION_KEY,PASSWORD,USER_TOKEN,INSTANCE_ID,AUTH_METHOD,ENV_TEMPLATE_ID) 
 values (1, 'test', 'TestSIS', 'Password1', null, null, 'Basic', 'DEV_LOCAL');
+
+
+-- -----------------------------------------------------
+-- Insert for Default Job Template
+-- -----------------------------------------------------
+-- Consumer
+INSERT INTO SIF3_JOB_TEMPLATE (JOB_TEMPLATE_ID,JOB_URL_NAME,ADAPTER_TYPE,TEMPLATE_FILE_NAME) 
+VALUES (1,'RolloverStudents','CONSUMER','rolloverStudentJob.xml');
+
+-- Provider: DIRECT
+INSERT INTO SIF3_JOB_TEMPLATE (JOB_TEMPLATE_ID,JOB_URL_NAME,ADAPTER_TYPE,TEMPLATE_FILE_NAME) 
+VALUES (2,'RolloverStudents','ENVIRONMENT_PROVIDER','rolloverStudentJob.xml');
+
+-- Provider: BROKERED
+INSERT INTO SIF3_JOB_TEMPLATE (JOB_TEMPLATE_ID,JOB_URL_NAME,ADAPTER_TYPE,TEMPLATE_FILE_NAME) 
+VALUES (3,'RolloverStudents','PROVIDER','rolloverStudentJob.xml');
