9ca5536078f04bbbc1fc6c188d7dd5c272ccd1b8
Joerg Huber <joerg.huber@systemic.com.au>
2018-08-23 12:32:15 +0800

    Changed data type of Job Init Parameters.

--- SIF3InfraREST/sif3Common/src/main/java/sif3/common/model/job/JobCreateRequestParameter.java
+++ SIF3InfraREST/sif3Common/src/main/java/sif3/common/model/job/JobCreateRequestParameter.java
@@ -18,13 +18,22 @@
 package sif3.common.model.job;
 
 import java.io.Serializable;
-import java.util.List;
 
-import sif3.common.model.AttributeValue;
+import org.w3c.dom.Element;
 
 /**
+ * As part of creating a job by a consumer the consumer can pass some "initialising" values to the job. The initial values 
+ * relate to a particular phase and can hold any data structure. The "any data structure" is indicated by an xs:anyType in
+ * the infrastructure data model for a job.
+ * Because this framework uses JAXB as its underlying serialiser for the infrastructure data model the xs:anyType is encapsulated
+ * into a org.w3c.dom.Element. This class abstracts the initialisation part of the Job object accordingly.
+ * 
+ * The "unfortunate" side effect of having an xs:anyType in the initialisation section of a Job object is that consumers and
+ * providers have to programmatically inspect the Element to extract actual parameter data. The framework cannot further 
+ * support that as an xs:anyType can be of any structure and depends entirely on the implementation of the functional service
+ * what that structure is. The framework is agnostic to this initialisation structure.
+ * 
  * @author Joerg Huber
- *
  */
 public class JobCreateRequestParameter implements Serializable
 {
@@ -32,14 +41,15 @@ public class JobCreateRequestParameter implements Serializable
     
     private String jobID = null;
     private String initPhaseName = null;
-    private List<AttributeValue> initParams = null;
+//    private List<AttributeValue> initParams = null;
+    private Element initParams = null;
     
     public JobCreateRequestParameter()
     {
         this(null, null);
     }
 
-    public JobCreateRequestParameter(String initPhaseName, List<AttributeValue> initParams)
+    public JobCreateRequestParameter(String initPhaseName, Element initParams)
     {
         super();
         setInitParams(initParams);
@@ -66,12 +76,12 @@ public class JobCreateRequestParameter implements Serializable
         this.initPhaseName = initPhaseName;
     }
     
-    public List<AttributeValue> getInitParams()
+    public Element getInitParams()
     {
         return initParams;
     }
     
-    public void setInitParams(List<AttributeValue> initParams)
+    public void setInitParams(Element initParams)
     {
         this.initParams = initParams;
     }
